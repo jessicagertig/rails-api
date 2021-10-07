a6ac8f2a7be5518a9bfa8cb11e361e18ca07280f
Jessica Cooper <jessicagcooper@gmail.com>
2021-10-06 21:45:06 -0500

    Basic test.

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "tests article object" do
+    # article = Article.create({title: 'Sample article', content: 'Sample content'})
+    article = create(:article)
+    expect(article.title).to eq('Sample article')
+  end
 end
