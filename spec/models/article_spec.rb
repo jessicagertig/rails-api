d5903fe74ef178ed65ae3e0c9128cf3026b58db9
Jessica Cooper <jessicagcooper@gmail.com>
2021-10-06 21:56:15 -0500

    Improve test syntax etc.

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it "tests article object" do
-    # article = Article.create({title: 'Sample article', content: 'Sample content'})
-    article = create(:article)
-    expect(article.title).to eq('Sample article')
+  describe '#validations' do
+    let(:article) { build(:article) }#create would save to db, but checking before saved so build is faster
+
+    it "tests that factory is valid" do
+      # article = Article.create({title: 'Sample article', content: 'Sample content'})
+      expect(article).to be_valid # article.valid? == true
+    end
+
+    it 'has an invalid title' do
+      article.title = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include ("can't be blank")
+    end
   end
 end
