bdb6e5e565f5ff1dc6fd8102b6cf300a223f6b59
Jessica Cooper <jessicagcooper@gmail.com>
2021-10-12 20:57:40 -0500

    Add pagination gem, extract all pagination logic to module (from controller).

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,28 @@
 class ArticlesController < ApplicationController
+  #all of the commented out lines below are logic extracted into the Paginable module --> there 'collection' is the var for 'articles' or any other controller
+  include Paginable 
   def index
-    articles = Article.all.recent
-    render json: serializer.new(articles), status: :ok
+    # articles = Article.recent
+    # paginated = paginator.call(
+    #   articles,
+    #   params: pagination_params,
+    #   base_url: request.url
+    # )
+    paginated = paginate(Article.recent)
+    # options = { meta: paginated.meta.to_h, links: paginated.links.to_h }
+    # render json: serializer.new(paginated.items, options), status: :ok
+    render_collection(paginated)
   end
 
   def serializer
     ArticleSerializer
   end
+
+  # def paginator
+  #   JSOM::Pagination::Paginator.new
+  # end
+
+  # def pagination_params
+  #   params.permit![:page]
+  # end
 end
