d389daa483bee521d9641e8a04ee88ba3ba9aaf9
Jessica Cooper <jessicagcooper@gmail.com>
2021-10-12 21:22:28 -0500

    Added show method to articles controller & requests tests.

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -66,4 +66,29 @@ RSpec.describe ArticlesController do
       )
     end
   end
+
+  describe '#show' do
+    let(:article) { create :article }
+
+    subject { get "/articles/#{article.id}" }
+
+    before { subject }
+
+    it 'returns a success response' do
+      # expect(response.status).to eq(200)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns a proper JSON response' do
+      aggregate_failures do
+        expect(json_data[:id]).to eq(article.id.to_s)
+        expect(json_data[:type]).to eq('articles')
+        expect(json_data[:attributes]).to eq(
+          title: article.title,
+          content: article.content,
+          slug: article.slug
+        )
+      end
+    end
+  end
 end
