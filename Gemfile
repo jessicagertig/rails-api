b656adf91ccc2dae65bbb28cb99b50e051a20b93
Jessica Cooper <jessicagcooper@gmail.com>
2021-10-13 20:02:00 -0500

    Add and implement JSONapi error gem.

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'puma', '~> 5.0'
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'jsonapi-serializer'
 gem 'jsom-pagination'
+gem 'jsonapi_errors_handler'
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,8 @@ class ArticlesController < ApplicationController
   def show
     article = Article.find(params[:id])
     render json: serializer.new(article)
+  # rescue ActiveRecord::RecordNotFound => e
+  #   render json: { message: e.message, detail: "Here we have a nicely formatted response"}
   end
 
   def serializer
